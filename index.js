269c37321a954728fb36409addfafd15326beba4
Ethan Maynard <ethan.maynard@sjsu.edu>
2019-09-18 09:53:28 -0700

    init bot

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import { Client } from 'discord.js';
-const client = new Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
   }
 });
 
-client.login('token');
+client.login("NjIzOTIzMTU1NTI1OTU5NzIw.XYJfxw.PScGadEfthdcJqI8KgnlETwSsq0");
+
+// https://discordapp.com/oauth2/authorize?client_id=623923155525959720&scope=bot
