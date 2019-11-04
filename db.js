b2cdec60b8b7dbf20cef04887ca634cac7234daf
Ethan Maynard <ethan.maynard@sjsu.edu>
2019-11-04 10:01:28 -0800

    stuff

--- db.js
+++ db.js
@@ -8,13 +8,6 @@ const connection = mysql.createConnection({
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "bugbot"
-
-  // var connection = mysql.createConnection({
-  //     host     : 'database-1.czh25xqqv727.us-west-1.rds.amazonaws.com',
-  //     user     : 'celestai69',
-  //     password : 'pPw4Efj9dEnwndKS',
-  //     database: 'bugbot',
-  //     port     : 3306
 });
 
 connection.connect(function(err) {
