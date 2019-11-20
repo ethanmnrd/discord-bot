ac03c903160c723947c04be77674b1026236d71e
Ethan Maynard <ethan.maynard@sjsu.edu>
2019-11-20 15:34:58 -0800

    refactor

--- commands/set.js
+++ commands/set.js
@@ -1,18 +1,17 @@
 const Discord = require("discord.js");
 var db = require("../db"); //connection
 const sqlFunctions = require("../requires/sql.js"); //functions
-var { terms } = require("../config.json");
 
 const maxVotes = 100;
 const minVotes = 1;
 
 const maxTime = 300000;
-const minTime = 1000
+const minTime = 1000;
 
 // For Rich Embeds
 
 function doSet(message, option, new_option) {
-  sqlFunctions.test();
+  // sqlFunctions.test();
   if (!option) return "Needs something to set!";
   if (!new_option) return "Needs a new setting for option!";
   switch (option) {
@@ -23,7 +22,7 @@ function doSet(message, option, new_option) {
       setTime(message, new_option);
       break;
     case "terms":
-      setTerms(args.join(" "));
+      setTerms(message, new_option);
       break;
     case "prefix":
       setPrefix(message, new_option);
@@ -38,21 +37,19 @@ function setVotes(message, amount) {
   if (!Number.isInteger(amount)) {
     return "Vote option must be set to an integer!";
   }
-  
-  if(amount < minVotes || amount > maxVotes){
-      message.channel.send('Votes cannot be lower than ' + minVotes + ' or higher than ' + maxVotes + '.');
-      return;
+
+  if (amount < minVotes || amount > maxVotes) {
+    message.channel.send(
+      "Votes cannot be lower than " +
+        minVotes +
+        " or higher than " +
+        maxVotes +
+        "."
+    );
+    return;
   }
-  
-  let sql = "CALL setVotesNeeded(" + message.guild.id + "," + amount + ");";
-  var waitForQuery = sqlFunctions.sqlPromise(message, sql, "Error setting votes");
-  waitForQuery
-    .then(result => {
-      sqlFunctions.viewSettings(message);
-    })
-    .catch(error => {
-      sqlFunctions.sayDatabaseError(message, error);
-    });
+
+  sqlFunctions.callProcedure("setVotesNeeded", message.guild.id, amount);
 }
 
 function setTime(message, amount) {
@@ -61,20 +58,18 @@ function setTime(message, amount) {
     return "Time must be set to a number (milliseconds)";
   }
 
-   if(amount < minTime || amount > maxTime){
-      message.channel.send('Time cannot be lower than ' + minTime + ' milliseconds or higher than ' + maxTime + ' milliseconds.');
-      return;
+  if (amount < minTime || amount > maxTime) {
+    message.channel.send(
+      "Time cannot be lower than " +
+        minTime +
+        " milliseconds or higher than " +
+        maxTime +
+        " milliseconds."
+    );
+    return;
   }
 
-  let sql = "CALL setTimeNeeded(" + message.guild.id + "," + amount + ");";
-  var waitForQuery = sqlFunctions.sqlPromise(message, sql, "Error setting votes");
-  waitForQuery
-    .then(result => {
-      sqlFunctions.viewSettings(message);
-    })
-    .catch(error => {
-      sqlFunctions.sayDatabaseError(message, error);
-    });
+  sqlFunctions.callProcedure("setTimeNeeded", message.guild.id, amount);
 }
 
 function setPrefix(message, new_prefix) {
@@ -84,22 +79,17 @@ function setPrefix(message, new_prefix) {
     if (is_num_or_letter(new_prefix)) {
       return `Prefix cannot be letters or numbers.`;
     } else {
-      let sql = `CALL setGuildPrefix(${message.guild.id}, "${new_prefix}");`;
-      var waitForQuery = sqlFunctions.sqlPromise(message, sql, "Error setting prefix");
-      waitForQuery
-        .then(result => {
-          sqlFunctions.viewSettings(message);
-        })
-        .catch(error => {
-          sqlFunctions.sayDatabaseError(message, error);
-        });
+      sqlFunctions.callProcedure(
+        "setGuildPrefix",
+        message.guild.id,
+        new_prefix
+      );
     }
   }
 }
 
-function setTerms(new_terms) {
-  terms = new_terms;
-  return `Terms have been set.`;
+function setTerms(message, new_terms) {
+  sqlFunctions.callProcedure("setGuildTerms", message.guild.id, new_terms);
 }
 
 // Checks if char is a num or letter (not allowed)
@@ -118,6 +108,6 @@ module.exports = {
   serverOnly: true,
   usage: "<option> <new_option>",
   execute(message, args) {
-    doSet(message, args[0], args[1]);
+    return message.channel.send(doSet(message, args[0], args.join(" ")));
   }
 };
